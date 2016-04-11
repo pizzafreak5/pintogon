ee22f5ea5210f43594fcd5471b86ab629c6ef5ba
Chris Delta <Chris Delta>
2016-04-11 13:29:23 -0600

    Nurble Nurble

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -5,6 +5,8 @@
 #include <list.h>
 #include <stdint.h>
 
+/*Bullshit change to demonstrate git to G, TODO delete later*/
+
 /* States in a thread's life cycle. */
 enum thread_status
   {
@@ -89,6 +91,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
